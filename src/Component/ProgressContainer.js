ef6f14e9f3dbb2ce150329fc5f3b82317d105055
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-04-03 14:30:41 +0530

    project completed

--- src/Component/ProgressContainer.js
+++ src/Component/ProgressContainer.js
@@ -2,18 +2,31 @@ import { connect } from 'react-redux'
 import { mapStateToProps, mapDispatchToProps } from '../redux-store/mapStore'
 // import { useRef } from 'react'
 const ProgressContainer =(props)=>{
-    // const bg_progress = useRef();
-    // console.log(bg_progress.current) 
-    return(
-        <div className="progress__container">
-            <div className="bg__progress"></div>
-            <div className="inner__container">
-                <div className="precent__container">
-                    <span className="percentage" id="progressPercent">{props.percent}</span>
+    if(props.percent < 100){
+        return(
+            <div className="progress__container">
+                <div className="bg__progress"></div>
+                <div className="inner__container">
+                    <div className="precent__container">
+                        <span className="percentage" id="progressPercent">File Uploading... {props.percent}%</span>
+                    </div>
                 </div>
+                <div className="progress__bar"></div>
             </div>
-            <div className="progress__bar"></div>
-        </div>
-    )
+        )
+    }else{
+        return(
+            <div className="progress__container">
+                <div className="bg__progress"></div>
+                <div className="inner__container">
+                    <div className="precent__container">
+                        <span className="percentage" id="progressPercent">File Uploaded</span>
+                    </div>
+                </div>
+                <div className="progress__bar"></div>
+            </div>
+        )
+    }
+    
 }
 export default connect(mapStateToProps, mapDispatchToProps)(ProgressContainer);
